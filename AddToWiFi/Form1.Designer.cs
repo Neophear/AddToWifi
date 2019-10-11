41c60980127f2e753680b9e3b89c5d5b5c1856c0
Stiig Gade <stiiggade@gmail.com>
2019-10-11 07:48:16 +0200

    All the changes

---
+++ AddToWiFi/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AddToWiFi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 41);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(181, 85);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text- and CSV-files|*.txt;*.csv|All files|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Åben fil";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 138);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(221, 172);
+            this.Name = "Form1";
+            this.Text = "Add users to Guest-WiFi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFile;
+    }
+}
+
---
+++ AddToWiFi/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddToWiFi
+{
+    public partial class Form1 : Form
+    {
+        private string LDAPDomainName = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!CheckIfInDomain())
+            {
+                MessageBox.Show("You are not part of a domain.\nThis application will now terminate.");
+                Environment.Exit(1);
+            }
+
+            LDAPDomainName = GetDomainDN(IPGlobalProperties.GetIPGlobalProperties().DomainName);
+
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+        public bool CheckIfInDomain()
+        {
+            bool isInDomain = true;
+
+            try
+            {
+                Domain.GetComputerDomain();
+            }
+            catch (ActiveDirectoryObjectNotFoundException)
+            {
+                isInDomain = false;
+            }
+
+            return isInDomain;
+        }
+        private string GetDomainDN(string domain)
+        {
+            DirectoryContext context = new DirectoryContext(DirectoryContextType.Domain, domain);
+            Domain d = Domain.GetDomain(context);
+            DirectoryEntry de = d.GetDirectoryEntry();
+            return de.Properties["DistinguishedName"].Value.ToString();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void AddUserToWiFi(string[] Usernames)
+        {
+            try
+            {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "INTERNET.LOCAL"))
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(pc, "Guest-WiFi");
+                    foreach (string user in Usernames)
+                    {
+                        try
+                        {
+                            group.Members.Add(pc, IdentityType.SamAccountName, user);
+
+                            //only runs if no exception caught
+                            Output(string.Format("+ {0} blev tilføjet til Guest-WiFi", user));
+                        }
+                        catch (PrincipalExistsException)
+                        {
+                            Output(string.Format("- {0} er allerede medlem af Guest-WiFi", user));
+                        }
+                        catch (NoMatchingPrincipalException)
+                        {
+                            Output(string.Format("% {0} findes ikke", user));
+                        }
+                    }
+                    group.Save();
+                }
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                Output(E.Message.ToString());
+            }
+        }
+
+        private void Output(string message)
+        {
+            txtOutput.AppendText(message + Environment.NewLine);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            ReadFile(openFileDialog1.FileName);
+        }
+
+        private void ReadFile(string FileName)
+        {
+            switch (Path.GetExtension(FileName))
+            {
+                case ".csv":
+                    List<string> lines = new List<string>(File.ReadAllLines(FileName));
+                    List<string> userList = new List<string>();
+                    lines.RemoveAt(0);
+
+                    foreach (string line in lines)
+                    {
+                        int indexOfSeperator = line.IndexOf(';');
+
+                        if (indexOfSeperator != -1)
+                        {
+                            userList.Add(line.Substring(0, indexOfSeperator));
+                        }
+                        else
+                        {
+                            userList.Add(line);
+                        }
+                    }
+
+                    AddUserToWiFi(userList.ToArray());
+                    break;
+                case ".txt":
+                    AddUserToWiFi(File.ReadAllLines(FileName));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (var file in files)
+                {
+                    if (Path.GetExtension(file) == ".txt" || Path.GetExtension(file) == ".csv")
+                    {
+                        ReadFile(file);
+                    }
+                }
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+}
